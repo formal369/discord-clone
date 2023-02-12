7cb22437d33cddc6f2a156b8f30d7b9f5d453d26
formal <rlaxodud9028@gmail.com>
2023-02-12 23:58:26 +0900

    setting redux

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore} from "firebase/firestore/lite";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYsSu6iqKAom0i0iv1BkwN-sLJYEHCeQ4",
+  authDomain: "discord-clone-2feff.firebaseapp.com",
+  projectId: "discord-clone-2feff",
+  storageBucket: "discord-clone-2feff.appspot.com",
+  messagingSenderId: "1096153602064",
+  appId: "1:1096153602064:web:a14039719214c410edd295"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
